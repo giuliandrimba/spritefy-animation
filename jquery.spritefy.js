8cd7d840217447d8ca17325b13d3bd6acde4ed56
Giulian Drimba <giuliandrimba@giuliandrimba.local>
2012-06-23 22:14:16 -0300

    Fixed various bugs and performances issues

--- jquery.spritefy.js
+++ jquery.spritefy.js
@@ -5,74 +5,91 @@
 	 * 
 	*/
 	
-	$.fn.spritefy = function(animation_name,options)
+	$.fn.spritefy = function(animation_name,p_options)
 	{
-		var options = options || {};
+		var options = p_options;
 		var el = this;
 
-		applyOptions()
+		resetElement();
+		el.removeClass();
+		el.addClass(animation_name);
+
+		applyBaseCSS();
+		applyOptions();
 
 		var api = {}
 
 		api.play = function()
 		{
-			if(!$(el).hasClass(animation_name))
-				$(el).addClass(animation_name);
-
-			if($(el).hasClass(animation_name+"-pause"))
-				$(el).removeClass(animation_name+"-pause");
-
-			if(!$(el).hasClass(animation_name+"-play"))
-				$(el).addClass(animation_name+"-play");
+			api.status = "playing";
+			applyCSS("animation-play-state","running");
 
 			return api;
 		}
 
 		api.pause = function()
 		{
-			if($(el).hasClass(animation_name+"-play"))
-				$(el).removeClass(animation_name+"-play");
-
-			if(!$(el).hasClass(animation_name+"-pause"))
-				$(el).addClass(animation_name+"-pause");
+			api.status = "paused";
+			applyCSS("animation-play-state","paused");
 
 			return api;
 		}
 
-		api.onStart = function()
-		{
+		el.bind("animationstart", onAnimation);  
+		el.bind("webkitAnimationStart", onAnimation);
 
-		}
 
-		api.onEnd = function()
-		{
+		el.bind("animationend", onAnimation);  
+		el.bind("webkitAnimationEnd", onAnimation);
 
-		}
-
-		api.onIteration = function()
-		{
-
-		}
-
-		el.get(0).addEventListener("animationstart", onAnimation, false);  
-		el.get(0).addEventListener("animationend", onAnimation, false);  
-		el.get(0).addEventListener("animationiteration", onAnimation, false);
+		el.bind("animationiteration", onAnimation);
+		el.bind("webkitAnimationIteration", onAnimation);
 
 		function onAnimation(e) 
 		{  
 			switch(e.type) 
 			{
 		    	case "animationstart":
-		    		api.onStart();
+		    		triggerStart();
+		    			break; 
+		    	case "webkitAnimationStart":
+		    		triggerStart();
 		    		break;  
-		    	case "animationend":  
-		      		api.onEnd();
+		    	case "animationend": 
+		    		triggerComplete();
+		    		break;
+		    	case "webkitAnimationEnd":
+		      		triggerComplete();
 		      		break;  
 		    	case "animationiteration":  
-		      		api.onIteration();
+			    	triggerIteration();
+			      		break;
+		    	case "webkitAnimationIteration":
+		      		triggerIteration();
 		      		break;  
 		  	}  
 		} 
+
+		function triggerComplete()
+		{
+			el.removeAttr("style");
+			if(options.onComplete)
+			{
+				options.onComplete();
+			}
+		}
+
+		function triggerIteration(time)
+		{
+			if(options.onIteration)
+				options.onIteration();
+		}
+
+		function triggerStart()
+		{
+			if(options.onStart)
+				options.onStart();
+		}
 
 		function applyOptions()
 		{
@@ -92,13 +109,36 @@
 			}
 		}
 
+		function applyBaseCSS()
+		{
+
+			applyCSS("animation-name", animation_name);
+			applyCSS("animation-timing-function","step-end");
+			applyCSS("animation-iteration-count","infinite");
+			applyCSS("animation-fill-mode","backwards");
+		}
+
 		function applyCSS(property, value)
 		{
-			$(el).css("-webkit-"+property,value);
-			$(el).css("-moz-"+property,value);
+			el.css("-webkit-"+property,value);
+			el.css("-moz-"+property,value);
+		}
+
+		function resetElement()
+		{
+			el.removeClass(animation_name);
+
+			el.unbind("animationstart");
+			el.unbind("webkitAnimationStart");
+			el.unbind("animationend");  
+			el.unbind("webkitAnimationEnd"); 
+			el.unbind("animationiteration");
+			el.unbind("webkitAnimationIteration");
 		}
 
 		$.fn.animation = api;
+
+		api.pause();
 
 		return api;
 	}
